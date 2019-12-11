8271309288c04dfec38763ba6a41dfad11c32f4a
David B <d.jbradley.ii@gmail.com>
2019-12-10 21:47:20 -0800

    adds logic to present upcoming events that the user created

--- src/UserDashboard/UserDashboard.js
+++ src/UserDashboard/UserDashboard.js
@@ -5,6 +5,25 @@ import './UserDashboard.css';
 
 export default class UserDashboard extends React.Component{
     render(){
+        const userId = parseInt(localStorage.getItem("userId"));
+        const users = this.props.users;
+        const user = users.find((user)=>{
+            return user.id === userId;
+        }); 
+
+        let events;
+
+        if(user.events.length === 0){
+            events = <tr><td>No Events Found</td></tr>;
+        }else{
+            events = user.events.map((eventId, i) => {
+                const userEvent = EVENTS.find((event, i) => {
+                    return event.id === eventId;
+                });
+                events = <tr><td>{userEvent.title}</td><td>{userEvent.organizer}</td><td>{userEvent.purpose}</td><td>{userEvent.restaurant}</td><td>{userEvent.date}</td></tr>; 
+                return events;
+            });
+        }
         return(
             <div className="user-dashboard">
                 <section className="user-upcoming-events">
