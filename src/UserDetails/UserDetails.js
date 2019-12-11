e4a21c83889bee3424ad164f13c31da2c9084267
David B <d.jbradley.ii@gmail.com>
2019-12-10 21:41:45 -0800

    adds user details to table

---
+++ src/UserDetails/UserDetails.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './UserDetails.css';
+
+function UserDetails(props){
+
+    return(
+        <section className="user-details">
+            <table className="user-details-table">
+                <thead><tr><th>Details:</th></tr></thead>
+                <tbody>
+                    <tr>
+                        <td>Name:</td><td>{fullName(user)}</td>
+                    </tr>
+                    <tr>
+                        <td>Email address:</td><td>{encryptEmail(user)}</td>
+                    </tr>
+                    <tr>
+                        <td>Occupation:</td><td>{user.occupation}</td>
+                    </tr>
+                    <tr>
+                        <td>Status:</td><td>{user.status}</td>
+                    </tr>
+                    <tr>
+                        <td>Summary:</td><td>{user.summary}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    );
+}
+
+export default UserDetails;
