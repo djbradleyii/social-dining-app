b582294e144106c25367688928ea895fd6ab5f89
David B <d.jbradley.ii@gmail.com>
2019-12-10 21:22:18 -0800

    adds form submit function and stores userID and authorization data to local storage

--- src/SignInPage/SignInPage.js
+++ src/SignInPage/SignInPage.js
@@ -2,7 +2,33 @@ import React from 'react';
 import './SignInPage.css';
 
 export default class SignInPage extends React.Component{
+    
+    formSubmit = e => {
+        e.preventDefault();
+        const { email, password } = e.target;
+        const { users } = this.props;
 
+        const emailFound = users.find((user, i) => {
+            return user.email.toLowerCase() === email.value.toLowerCase();
+        });
+
+        const passwordFound = users.find((user, i) => {
+            return user.password.toLowerCase() === password.value.toLowerCase();
+        });
+
+        if(!!emailFound && !!passwordFound){
+            const user = users.find((user) => {
+                return user.email.toLowerCase() === email.value.toLowerCase()
+            });
+            localStorage.setItem("authorized","yes");
+            localStorage.setItem("userId", user.id);
+            this.props.history.push(`/dashboard/${user.id}`);
+        }else{
+            localStorage.setItem("authorized", "no");
+            email.value = '';
+            password.value = '';
+        }
+    }
     render(){
         return(
             <form onSubmit={this.formSubmit} id="signin-form">
