f00c78897187bcebdabc80da5a8e160645bdc49d
David B <d.jbradley.ii@gmail.com>
2019-12-16 15:53:06 -0800

    updates App path and removes users test file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../src/components/App';
 import { BrowserRouter } from 'react-router-dom';
-import USERS from './users';
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App users={USERS}/>
+        <App />
     </BrowserRouter>, 
     document.getElementById('root')
 );
