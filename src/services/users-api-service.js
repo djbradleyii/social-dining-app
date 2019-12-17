d93c4dca9c1b6a22d2d2e4d3f8ec6f199f29838a
David B <d.jbradley.ii@gmail.com>
2019-12-16 17:10:37 -0800

    Adds postEvent method to POST /events endpoint for users api service

--- src/services/users-api-service.js
+++ src/services/users-api-service.js
@@ -42,8 +42,25 @@ const UsersApiService = {
             return response.json();
           })
       },
-      
-      
+      postEvent(userId, newEvent) {
+        return fetch(`${config.API_ENDPOINT}/events`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${TokenService.getAuthToken()}`,
+          },
+          body: JSON.stringify({
+            user_id: userId,
+            newEvent,
+          }),
+        })
+          .then(response =>{
+            if(!response.ok){
+              throw new Error('Something went wrong. Please try again later.');
+            }
+            return response.json();
+          })
+      } 
 }
 
 export default UsersApiService;
