9c1069909b348b9e869757cbbe02f09b946eebf7
David B <d.jbradley.ii@gmail.com>
2019-12-16 17:06:04 -0800

    Adds getUsers method to GET users from /users endpoint users api service

--- src/services/users-api-service.js
+++ src/services/users-api-service.js
@@ -2,7 +2,20 @@ import TokenService from '../services/token-service'
 import config from '../config'
 
 const UsersApiService = {
-
+    getUsers() {
+        return fetch(`${config.API_ENDPOINT}/users`, {
+            headers: {
+                'authorization': `Bearer ${TokenService.getAuthToken()}`
+            },
+        })
+          .then(response => {
+            if(!response.ok){
+              throw new Error('Something went wrong. Please try again later.');
+            }
+            return response.json();
+          })
+      },
+      
 }
 
 export default UsersApiService;
