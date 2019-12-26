107bdb86f82d33f0b91abde5b9152b6512419bea
David B <d.jbradley.ii@gmail.com>
2019-12-26 01:25:13 -0800

    Updates users-api-services to match the routes from the router

--- src/services/users-api-service.js
+++ src/services/users-api-service.js
@@ -2,7 +2,7 @@ import TokenService from '../services/token-service'
 import config from '../config'
 
 const UsersApiService = {
-    getUsers() {
+    getAllUsers() {
         return fetch(`${config.API_ENDPOINT}/users`, {
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`
@@ -15,8 +15,8 @@ const UsersApiService = {
             return response.json();
           })
       },
-      getUserById(userId) {
-        return fetch(`${config.API_ENDPOINT}/users/${userId}`, {
+      getUserById(user_id) {
+        return fetch(`${config.API_ENDPOINT}/users/${user_id}`, {
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`
             },
@@ -28,9 +28,9 @@ const UsersApiService = {
             return response.json();
           })
       },
-      getUsersEvents(userId) {
-          //fetch events for the individual
-        return fetch(`${config.API_ENDPOINT}/users/${userId}/events`, {
+      getAllEventsByUserId() {
+          //fetch events for the individual user
+        return fetch(`${config.API_ENDPOINT}/users/all/events`, {
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`
             },
@@ -42,16 +42,15 @@ const UsersApiService = {
             return response.json();
           })
       },
-      postEvent(userId, newEvent) {
-        return fetch(`${config.API_ENDPOINT}/events`, {
+      postUser(newUser) {
+        return fetch(`${config.API_ENDPOINT}/users`, {
           method: 'POST',
           headers: {
             'content-type': 'application/json',
             'authorization': `Bearer ${TokenService.getAuthToken()}`,
           },
           body: JSON.stringify({
-            user_id: userId,
-            newEvent,
+            newUser,
           }),
         })
           .then(response =>{
@@ -60,7 +59,44 @@ const UsersApiService = {
             }
             return response.json();
           })
-      } 
+      },
+      deleteUser() {
+        return fetch(`${config.API_ENDPOINT}/users`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${TokenService.getAuthToken()}`,
+          },
+          body: JSON.stringify({
+            id: userId,
+          }),
+        })
+          .then(response =>{
+            if(!response.ok){
+              throw new Error('Something went wrong. Please try again later.');
+            }
+            return response.json();
+          })
+      },
+      updateUserById(userId, userUpdates) {
+        return fetch(`${config.API_ENDPOINT}/users/${userId}`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${TokenService.getAuthToken()}`,
+          },
+          body: JSON.stringify({
+            userId,
+            userUpdates
+          }),
+        })
+          .then(response =>{
+            if(!response.ok){
+              throw new Error('Something went wrong. Please try again later.');
+            }
+            return response.json();
+          })
+      }  
 }
 
 export default UsersApiService;
