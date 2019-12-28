ac0f46fc8be917de05155d4d70f41549ebd8c16e
David B <d.jbradley.ii@gmail.com>
2019-12-28 02:26:53 -0800

    Updates the Link route for populate Organizer

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -54,7 +54,7 @@ export default class UserDashboard extends React.Component{
                                         actionButton = <button onClick={(e) => this.deleteEvent(e, event.event_id)}>Delete</button>;
                                     } else {
                                         let eventId = event.event_id;
-                                        populateOrganizer = <Link to={`/infoPage/${event.event_id}`}>{event.organizer}</Link>;
+                                        populateOrganizer = <Link to={`/info/${event.event_id}`}>{event.organizer}</Link>;
                                         actionButton = <button onClick={(e) => this.removeRSVP(e, eventId)}>Remove RSVP</button>
                                     } 
                                     return <tr key={i}><td><Link to={`/event/${event.event_id}`}>{this.trimText(event.title)}</Link></td><td>{this.trimText(populateOrganizer)}</td><td>{this.trimText(event.event_purpose)}</td><td>{this.trimText(event.restaurant)}</td><td>{moment(event.date).format('MM/DD/YY')}</td><td>{actionButton}</td></tr>;
