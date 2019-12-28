dc4c34dbb76a82e6aa0452e8782ed14041c269b9
David B <d.jbradley.ii@gmail.com>
2019-12-28 01:22:45 -0800

    Makes controlled form. Utilizing case sensitive endpoint to search events by all event text fields

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,79 +1,93 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SearchPage.css';
 import ContextManager from '../../context/context-manager';
-
+import EventsApiService from '../../services/events-api-service';
+import moment from 'moment';
+import ActiveUserService from '../../services/activeuser-service';
 
 export default class SearchPage extends React.Component{
+     static contextType = ContextManager;
+                       
+    state = {
+        keyword: '',
+        events: []
+    }
+
+    updateKeyword = (e) => {
+        e.preventDefault();
+        this.setState({
+            keyword: e.target.value
+        })
+        EventsApiService.getEventByKeyword(this.state.keyword)
+        .then(events => {
+            this.setState({
+                events
+            })
+        })
+        if(this.state.keyword.length === 0){
+            EventsApiService.getAllEvents()
+            .then(events => {
+                this.setState({
+                    events
+                })
+            })
+        }
+    }
+
+    renderContent(){
+        let events = this.state.events;
+        let populateOrganizer;
+        let activeuser = ActiveUserService.getUserData();
+        let user = activeuser.user;
+        events = events.map((event, i) => {
+            if(event.organizer === user.id){
+                populateOrganizer = 'Me';
+            } else {
+                populateOrganizer = <Link to={`/info/${event.organizer}`}>{event.organizer_name}</Link>;
+            }
+            return <tr key={i}><td><Link to={`/event/${event.id}`}>{event.title}</Link></td><td>{populateOrganizer}</td><td>{event.event_purpose}</td><td>{event.restaurant}</td><td>{moment(event.date).format('MM/DD/YY')}</td></tr>
+        })
+
+        return(
+            <section className="events-list">
+                <table className="events-list-table">
+                    <thead><tr><th>Event List:</th></tr></thead>
+                    <tbody>
+                        <tr>
+                            <th>Title</th>
+                            <th>Organizer</th>
+                            <th>Purpose</th>
+                            <th>Restaurant</th>
+                            <th>Date</th>
+                        </tr>
+                        {events}
+                    </tbody>
+                </table>
+            </section>
+        )
+    }
+
+    componentDidMount(){
+        EventsApiService.getAllEvents()
+        .then(events => {
+            this.setState({
+                events
+            })
+        })
+    }
     render(){
+        console.log(this.state.keyword);
         return(
             <ContextManager.Consumer>
                 {(value) => {
-/*                             getEvents = () => {
-                                let { term, date, searchType} = this.state.searchOptions;
-                                let events = this.state.events;
-                        
-                                if(term){
-                                    if(searchType.toLowerCase() === "purpose".toLowerCase()){
-                                        events = events.filter((event) => {
-                                            return event.purpose.toLowerCase().includes(term.toLowerCase());
-                                        });
-                                    }else if(searchType.toLowerCase() === "restaurant".toLowerCase()){
-                                        events = events.filter((event) => {
-                                            return event.restaurant.toLowerCase().includes(term.toLowerCase());
-                                        });
-                                    }
-                                }
-                        
-                                if(date){
-                                    events = events.filter((event) => {
-                                        return event.date >= date;
-                                    });
-                                }
-                        
-                                events = events.map((event, i) => {
-                                    const eventPageId = event.id;
-                                    const eventPageURL = `/event/${eventPageId}`;
-                                return(
-                                    <tr>
-                                    <td>{event.title}</td><td>{event.organizer}</td><td>{event.purpose}</td><td>{event.restaurant}</td><td>{event.date}</td>
-                                    </tr>
-                                ); 
-                            });
-                        
-                                return events;
-                            }
-                        
-                            handleFormSubmission = e => {
-                                e.preventDefault();
-                                let { term, eventDate, searchType } = e.target;
-                                if(!term){
-                                    term = '';
-                                }
-                        
-                                if(!eventDate){
-                                    eventDate = '';
-                                }
-                        
-                                const searchOptions = {
-                                    term: term.value,
-                                    date: eventDate.value,
-                                    searchType: searchType.value
-                                };
-                        
-                                this.setState({searchOptions});
-                            } */
                     return(
                         <div>
-                            <form id="search-form">
+                            <form id="search-form" onSubmit={this.handleFormSubmission}>
                                 <div>
-                                    <label htmlFor="search">Search:</label>
-                                    <input type="search" id="search" placeholder="Wine &amp; Networking" name="term" />
+                                    <label htmlFor="search">Search <span className="searchCaseSensitive">(Case Sensitive):</span> </label>
+                                    <input type="search" id="search" placeholder="Wine &amp; Networking" value={this.state.keyword} name="keyword" onChange={this.updateKeyword}/>
                                     {/* <img src="" /> */}
-                                </div>
-                                <div>
-                                    <label className="search-type-label">Search type:</label>
-                                    <span className="search-type">Event Purpose <input type="radio" name="searchType" value="purpose" defaultChecked /></span>
-                                    <span className="search-type">Restaurant <input type="radio" name="searchType" value="restaurant" /></span>
                                 </div>
             {/*                     <div>
                                     <label htmlFor="event-date-search">Date:</label>
@@ -83,22 +97,8 @@ export default class SearchPage extends React.Component{
                                     <button type="submit">Submit</button>
                                 </div>    
                             </form>
-                            <section className="events-list">
-                                <table className="events-list-table">
-                                    <thead><tr><th>Event List:</th></tr></thead>
-                                    <tbody>
-                                        <tr>
-                                            <th>Title</th>
-                                            <th>Organizer</th>
-                                            <th>Purpose</th>
-                                            <th>Restaurant</th>
-                                            <th>Date</th>
-                                        </tr>
-                                        {/* this.getEvents() */}
-                                    </tbody>
-                                </table>
-                            </section>
-                    </div>
+                            {!!this.state.events.length && this.renderContent()}
+                        </div>
                     )
                 }}
             </ContextManager.Consumer>
