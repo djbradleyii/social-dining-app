a00b20a2fdaf30fc72ae8d8993d6c91987ab795c
David B <d.jbradley.ii@gmail.com>
2019-12-28 01:10:24 -0800

    Updates input capture. Adds moments to date/time. Requires the appropriate fields. Updates input field values to match db ENUM

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -1,71 +1,70 @@
 import React from 'react';
 import './AddEvent.css';
+import EventsApiService from '../../services/events-api-service';
+import moment from 'moment';
+import ContextManager from '../../context/context-manager';
 
 export default class AddEvent extends React.Component{
+    static contextType = ContextManager;
+    
     handleFormSubmit = e => {
         e.preventDefault();
-        const { restaurant, restaurantAddress, eventDescription, eventDate, purpose } = e.target;
-        //const { title, ageRange} = e.target;
-/*         const userId = parseInt(localStorage.getItem("userId"));
-        const users = this.props.users;
-        const user = users.find((user) => {
-            return user.id === userId;
-        });
-
+        const { title, restaurant, restaurantAddress, eventDescription, eventDate, purpose } = e.target;
+        const event_date = new Date(eventDate.value);
         const newEvent = {
-            id: EVENTS.length + 1,
-            organizer : user.fname,
-            purpose: purpose.value,
+            title: title.value,
+            event_purpose: purpose.value,
             restaurant: restaurant.value,
             address : restaurantAddress.value,
-            date : eventDate.value,
-            time : "05:30PM",
+            date : moment(event_date).format('MM/DD/YYYY'),
+            time : moment(event_date).format('hh:mm A'),
             description : eventDescription.value,
-            singlesOnly : "No",
-            attendees : 0  
         }
 
-        user.events.push(newEvent.id);
-        EVENTS.push(newEvent);  
-        localStorage.setItem("eventId", newEvent.id);
-        this.props.history.push(`/event/${newEvent.id}`); */
+        if(purpose.value.toLowerCase().includes('singles')){
+            newEvent.singles_only = true;
+        } else {
+            newEvent.singles_only = false;
+        }
+
+        EventsApiService.postEvent(newEvent)
+        .then((event) => {
+            this.context.getAllEventsForUser();
+            this.props.history.push('/dashboard');
+        })
     }
     render(){
         return(
             <form onSubmit={this.handleFormSubmit} id="add-event-form">
                 <div>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" placeholder="Wine & Networking" name="title" />
+                    <input type="text" id="title" placeholder="Wine & Networking" name="title" required />
                 </div>
                 <div>
                     <label htmlFor="restaurant">Restaurant:</label>
-                    <input type="text" id="restaurant" placeholder="Water Grill" name="restaurant" />
+                    <input type="text" id="restaurant" placeholder="Water Grill" name="restaurant" required />
                 </div>
                 <div>
                     <label htmlFor="restaurant-address">Restaurant Address:</label>
-                    <input type="text" id="restaurant-address" placeholder="123 Restaurant Lane" name="restaurantAddress" />
+                    <input type="text" id="restaurant-address" placeholder="123 Restaurant Lane" name="restaurantAddress" required/>
                 </div>
                 <div>
                     <label htmlFor="event-description">Description:</label>
-                    <textarea id="event-description" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis finibus orci, vel auctor dolor vulputate vitae. In fringilla tristique dui vitae blandit." name="eventDescription">
+                    <textarea id="event-description" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis finibus orci, vel auctor dolor vulputate vitae. In fringilla tristique dui vitae blandit." name="eventDescription" required>
                     </textarea>
                 </div>
                 <div>
                     <label htmlFor="event-date">Date:</label>
-                    <input type="datetime-local" id="event-date" name="eventDate" />
+                    <input type="datetime-local" id="event-date" name="eventDate" required />
                 </div>
                 <div>
                     <label htmlFor="purpose">Purpose:</label>
-                    <select id="purpose" name="purpose">
-                        <option value="social">Social</option>
-                        <option value="networking">Networking</option>
-                        <option value="game night">Game Night</option>
-                        <option value="singles night">Singles Night</option>
+                    <select id="purpose" name="purpose" required>
+                        <option value="Social">Social</option>
+                        <option value="Networking">Networking</option>
+                        <option value="Game Night">Game Night</option>
+                        <option value="Singles Night">Singles Night</option>
                     </select>
-                </div>
-                <div>
-                    <label htmlFor="age-range">Minimum Age Requirement:</label>
-                    <input type="number" id="age-range" min="18" max="99" name="ageRange" />
                 </div>
                 <div>
                     <button type="submit">Submit</button>
