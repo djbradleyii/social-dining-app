b6255b9d0bf336d097b2059b8a62bb70971b8685
David B <d.jbradley.ii@gmail.com>
2019-12-28 01:15:09 -0800

    Updates Components list and adds their routes. Removes unnecessary functions from context. Adds getAllEventsForUser, getSelectedEvent, and getAllEvents function logic

--- src/components/App.js
+++ src/components/App.js
@@ -10,81 +10,38 @@ import AddEvent from './AddEvent/AddEvent';
 import EventPage from './EventPage/EventPage';
 import Footer from './Footer/Footer';
 import PageNotFound from './PageNotFound/PageNotFound';
+import EditEvent from './EditEvent/EditEvent';
+import EditUser from './EditUser/EditUser';
 import './App.css';
 import ContextManager from '../context/context-manager';
 import TokenService from '../services/token-service';
-import config from '../config';
-import AuthApiService from '../services/auth-api-service';
+import ActiveUserService from '../services/activeuser-service';
+//import config from '../config';
+//import AuthApiService from '../services/auth-api-service';
+import UsersApiService from '../services/users-api-service';
+//import EventsApiService from '../services/events-api-service';
+//import AttendeesApiService from '../services/attendees-api-service';
+import EventsApiService from '../services/events-api-service';
+import SelectedUserDetails from './SelectedUserDetails/SelectedUserDetails';
 
 class App extends React.Component{
-
-
-/*     static defaultProps = {
-      color: '#01A800', // green
-    };
-    render() {
-       content not shown 
-    }
-  }
-  We're not going to explain the details of this syntax. The rule for making default props is to create a defaultProps property for the class with static defaultProps and then assign an object to that property. The keys of that object are the props you want to have defaults for and the values are the default values you want to assign. */
   constructor(props){
     super(props);
     this.state = {
-      users: [],
+      loggedInUserData: {},
       events: [],
-      attendees: [],
-      error: null,
+      selectedEvent: {},
+      error: null
     }
   }
 
-  addUser = () => {
-
-  }
-
-  editUser = () => {
-
-  }
-
-  deleteUser = () => {
-
-  }
-  
-  addEvent = () => {
-
-  }
-  
-  editEvent = () => {
-
-  }
-
-  deleteEvent = () => {
-
-  }
-
-  addAttendee = () => {
-
-  }
-
-  deleteAttendee = () => {
-
-  }
-
-  getAllUsers = () => {
-      return fetch(`${config.API_ENDPOINT}/users`, {
-        headers: {
-          'authorization': `Bearer ${TokenService.getAuthToken()}`
-        }
-      })
-      .then(response => {
-        if(!response.ok) {
-          throw new Error('Something went wrong, please try again later.')
-        }
-        return response.json();
-      })
-      .then(eventsData => {
+  getAllEventsForUser = () => {
+    UsersApiService.getAllEventsForUser()
+      .then(usersData => {
+        ActiveUserService.saveUserData(usersData)
         this.setState({
-          events: eventsData
-        })
+          loggedInUserData: usersData
+        })        
       })
       .catch(err => {
         this.setState({
@@ -93,21 +50,33 @@ class App extends React.Component{
       });
   }
 
+  getSelectedEvent = (event_id) => {
+    EventsApiService.getAllAttendeesByEventId(event_id)
+    .then((event) => { 
+      this.setState({
+        selectedEvent: event
+      })
+    })
+  }
+
+  updateKeyword = (keyword) => {
+    this.setState({
+      keyword
+    })
+    this.getAllEvents();
+  }
+
+  updateEventId = (event_id) => {
+    console.log(event_id);
+    this.getSelectedEvent(event_id);
+  }
+
   getAllEvents = () => {
-    return fetch(`${config.API_ENDPOINT}/events`, {
-      headers: {
-        'authorization': `Bearer ${TokenService.getAuthToken()}`
-      }
-    })
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('Something went wrong, please try again later.')
-      }
-      return response.json();
-    })
-    .then(usersData => {
+    const { keyword } = this.state;
+    EventsApiService.getAllEvents(keyword)
+    .then(eventsData => {
       this.setState({
-        users: usersData
+        events: eventsData
       })
     })
     .catch(err => {
@@ -117,70 +86,46 @@ class App extends React.Component{
     });
   }
 
-  getAllAttendees = () => {
-    return fetch(`${config.API_ENDPOINT}/users`, {
-      headers: {
-        'authorization': `Bearer ${TokenService.getAuthToken()}`
-      }
-    })
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('Something went wrong, please try again later.')
-      }
-      return response.json();
-    })
-    .then(attendeesData => {
-      this.setState({
-        attendees: attendeesData
-      })
-    })
-    .catch(err => {
-      this.setState({
-        error: err.message
-      });
-    });
-  }
-
-  handleRegistrationSuccess = (newUser) => {
-
+  componentDidMount(){
+    if(TokenService.hasAuthToken()){
+      this.getAllEventsForUser();
+      this.getAllEvents();
+    }
   }
 
   render(){
     const contextValue = {
-      users: this.state.users,
+      updateKeyword: this.updateKeyword,
+      loggedInUserData: this.state.loggedInUserData,
       events: this.state.events,
-      attendees: this.state.attendees,
-      getAllUsers: this.getAllUsers,
-      addUser: this.addUser,
-      editUser: this.editUser,
-      deleteUser: this.deleteUser,
+      eventAttendees: this.state.eventAttendees,
+      getSelectedEvent: this.getSelectedEvent,
+      selectedEvent: this.state.selectedEvent,
       getAllEvents: this.getAllEvents,
-      addEvent: this.addEvent,
-      editEvent: this.editEvent,
-      deleteEvent: this.deleteEvent,
-      getAllAttendees: this.getAllAttendees,
-      addAttendee: this.addAttendee,
-      deleteAttendee: this.deleteAttendee,
-      handleRegistrationSuccess: this.handleRegistrationSuccess
+      getAllEventsForUser: this.getAllEventsForUser, 
+      updateEventId: this.updateEventId
     }
     return(
       <ContextManager.Provider value={contextValue}>
         <div className='App'>
-          <Route path="/" component={Header} />
+          <Route path='/' component={Header} />
           <main aria-live='polite'>
             <Switch>
               <Route exact path='/' component={LandingPage} />
-              <Route exact path="/register" render={({history}) => {return <RegisterPage onRegistrationSuccess={this.handleRegistrationSuccess} />}} />
-              <Route exact path="/signin" component={SignInPage} />
-              <Route exact path="/dashboard/:userId" component={UserDashboard} />
-              <Route exact path="/search" component={SearchPage} />
-              <Route exact path="/event" component={AddEvent} />
-              <Route exact path="/event/:eventId" component={EventPage} />
-              <Route exact path="/signout" component={LandingPage} />
+              <Route exact path='/register' component={RegisterPage} />
+              <Route exact path='/signin' component={SignInPage} />
+              <Route exact path='/dashboard' component={UserDashboard} />
+              <Route exact path='/search' component={SearchPage} />
+              <Route exact path='/event' component={AddEvent} />
+              <Route exact path='/event/:eventId' component={EventPage} />
+              <Route exact path='/signout' component={LandingPage} />
+              <Route exact path='/edit/user/:userId' component={EditUser} />
+              <Route exact path='/edit/event/:eventId' component={EditEvent} />
+              <Route exact path='/info/:userId' component={SelectedUserDetails} />
               <Route component={PageNotFound} />
             </Switch>
           </main>
-          <Route path="/" component={Footer} />
+          <Route path='/' component={Footer} />
         </div>
       </ContextManager.Provider>
     );
