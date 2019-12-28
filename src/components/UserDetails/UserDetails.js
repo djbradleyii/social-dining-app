86809f399ac4f29f80baf52ea983527fddbf12f7
David B <d.jbradley.ii@gmail.com>
2019-12-28 01:33:23 -0800

    Adds edit button to edit user data. Updates logic to use ActiveUserService

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -1,53 +1,55 @@
 import React from 'react';
 import './UserDetails.css';
+import { withRouter } from 'react-router-dom';
 import ContextManager from '../../context/context-manager';
+import ActiveUserService from '../../services/activeuser-service';
 
 class UserDetails extends React.Component{
+    static contextType = ContextManager;
+    editUser = (e, user_id) => {
+        const { history } = this.props;
+        history.push(`/edit/user/${user_id}`)
+    }
+
     render(){
         return(
             <ContextManager.Consumer>
                 {(value) => {
-/*                      const user = value.user;
-                     function fullName(user){
-                         return `${user.fname} ${user.lname}`;
-                     }
-                 
-                     function encryptEmail(user){
-                         let email = user.email;
-                         let indexOfAt = email.indexOf('@');
-                         let sliced = email.slice(1,indexOfAt-1);
-                         let regx = new RegExp(sliced,'gi');
-                         let hiddenEmail = email.replace(regx,'*'.repeat(sliced.length));
-                         return hiddenEmail;
-                     } */
-                    return(
-                        <section className="user-details">
-                            <table className="user-details-table">
-                                <thead><tr><th>Details:</th></tr></thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Name:</td><td>{/* fullName(user) */}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Email address:</td><td>{/* encryptEmail(user) */}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Occupation:</td><td>{/* user.occupation */}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Status:</td><td>{/* user.status */}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Summary:</td><td>{/* user.summary */}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </section>
-                    )
+                    let activeuser = ActiveUserService.getUserData();
+                    let user = activeuser.user;
+                    const nameDisplay = user.fname + ' ' + user.lname.charAt(0) + '.';
+                    user.fullName = nameDisplay
+                        return(
+                            <section className="user-details">
+                                <table className="user-details-table">
+                                    <thead><tr><th>Details:</th></tr></thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Name:</td><td>{ user.fullName }</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Email address:</td><td>{user.email}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Occupation:</td><td>{user.occupation}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Status:</td><td>{user.marital_status}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bio:</td><td>{user.bio}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><div className="button-group"><div className="button-container"><button onClick={(e) => this.editUser(e, user.id)}>Edit</button></div></div></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </section>
+                        )
                 }}
             </ContextManager.Consumer>
         )
     }
 }
 
-export default UserDetails;
+export default withRouter(UserDetails);
