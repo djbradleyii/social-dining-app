fa80c204463149947abc2f0fbf2ff921dca5bdcc
David B <d.jbradley.ii@gmail.com>
2020-01-05 10:26:26 -0800

    Adds clip text classes to appropriate columns

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -60,10 +60,10 @@ export default class UserDashboard extends React.Component{
                                         actionButton = <button className="action-btn" onClick={(e) => this.deleteEvent(e, event.event_id)}>Delete</button>;
                                     } else {
                                         let eventId = event.event_id;
-                                        populateOrganizer = <Link to={`/info/${event.organizer_id}`}>{event.organizer}</Link>;
+                                        populateOrganizer = <Link to={`/info/${event.organizer_id}` }>{event.organizer}</Link>;
                                         actionButton = <button className="action-btn" onClick={(e) => this.removeRSVP(e, eventId)}>Remove RSVP</button>
                                     } 
-                                    return <tr key={i}><td><Link to={`/event/${event.event_id}`}>{this.trimText(event.title)}</Link></td><td>{this.trimText(populateOrganizer)}</td><td className="m-hide">{this.trimText(event.event_purpose)}</td><td>{this.trimText(event.restaurant)}</td><td className="m-hide">{moment(event.date).format('MM/DD/YY')}</td><td>{actionButton}</td></tr>;
+                                    return <tr key={i}><td className="clipTitleText"><Link to={`/event/${event.event_id}`}>{event.title}</Link></td><td className="clipOrganizerText">{populateOrganizer}</td><td className="m-hide">{event.event_purpose}</td><td className="clipRestaurantText">{event.restaurant}</td><td className="m-hide">{moment(event.date).format('MM/DD/YY')}</td><td>{actionButton}</td></tr>;
                                 });
                             }
                     return(
