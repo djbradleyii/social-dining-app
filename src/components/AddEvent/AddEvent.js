68f9656c912ac8b64ac095543c6272a76a1ab814
David B <d.jbradley.ii@gmail.com>
2020-01-05 10:15:48 -0800

    Adds custom date functions with logic to fix iPhone compatibility with input date-time-local

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -6,11 +6,48 @@ import ContextManager from '../../context/context-manager';
 
 export default class AddEvent extends React.Component{
     static contextType = ContextManager;
+
+    addLeadingZero = (number) => {
+        return number < 10 ? `0${number}` : number;
+    }
+
+    isLeapYear = (year)  => {
+        if(year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)){
+            return true;
+        } else {
+            return false;
+        }
+    }
     
+    isValidDate = (month, day, year) => {
+        let isValid = null;
+        const validThirtyOneDays = ['January', 'March', 'May', 'July', 'August', 'October', 'December'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        if(months.includes(month) && day <= 30){
+            isValid = true;
+        }
+        
+        if(day === 31){
+            isValid = validThirtyOneDays.includes(month);
+        }
+
+        if(month === 'February' && day === 29){
+            isValid = this.isLeapYear(year);
+        } else if (month === 'February' && day > 28){
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
     handleFormSubmit = e => {
         e.preventDefault();
-        const { title, restaurant, restaurantAddress, eventDescription, eventDate, purpose } = e.target;
-        const event_date = new Date(eventDate.value);
+        const { title, restaurant, restaurantAddress, eventDescription, month, day, year, hour, minute, meridiem, purpose } = e.target;
+        const event_date = `${month.value}/${parseInt(day.value)}/${parseInt(year.value)} ${this.addLeadingZero(parseInt(hour.value))}:${this.addLeadingZero(parseInt(minute.value))} ${meridiem.value}`;
+
+        const isDate = this.isValidDate(month.value, parseInt(day.value), parseInt(year.value));
+
         const newEvent = {
             title: title.value,
             event_purpose: purpose.value,
@@ -27,17 +64,88 @@ export default class AddEvent extends React.Component{
             newEvent.singles_only = false;
         }
 
-        EventsApiService.postEvent(newEvent)
-        .then((event) => {
-            this.context.getAllEventsForUser();
-            this.props.history.push('/dashboard');
-        })        
-        .catch(res => {
-            this.context.updateErrorMessage('Oops: '+ res.error);
+        if(isDate){
+            EventsApiService.postEvent(newEvent)
+            .then((event) => {
+                this.context.clearErrorMessage();
+                this.context.getAllEventsForUser();
+                this.props.history.push('/dashboard');
+            })        
+            .catch(res => {
+                this.context.updateErrorMessage('Oops: '+ res.error);
+                this.context.scrollToErrorMessage();
+            })
+        } else {
+            this.context.updateErrorMessage('Oops: Invalid Date');
             this.context.scrollToErrorMessage();
-        })
+        }
     }
-    
+
+    dateInputCreator = () => {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+        const currentYear = new Date().getFullYear();        
+        const years = [currentYear, currentYear + 1]
+
+        const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+        const minutes = [ '00', 15, 30, 45 ];
+
+        const meridiems = ['AM', 'PM'];
+
+        const monthOptions = months.map((month, i) => {
+                return <option key={i} value={month}>{month}</option>
+            })
+
+        const dayOptions = days.map((day, i) => {
+            return <option key={i} value={day}>{day}</option>
+        })
+
+        const yearOptions = years.map((year, i) => {
+            return <option key={i} value={year}>{year}</option>
+        })
+
+        const hoursOptions = hours.map((hour, i) => {
+            return <option key={i} value={hour}>{hour}</option>
+        })
+
+        const minutesOptions = minutes.map((minute, i) => {
+            return <option key={i} value={minute}>{minute}</option>
+        })
+
+        const meridiemOptions = meridiems.map((meridiem, i) => {
+            return <option key={i} value={meridiem}>{meridiem}</option>
+        })
+
+        return (
+            <>
+                <div className="event-date">
+                    <label>Date:</label>
+                    <select className="date-month" name="month">
+                    <option value="None">Select Month:</option>
+                        {monthOptions}
+                    </select>
+                    <select className="date-day" name="day">
+                    <option value="None">Select Day:</option>
+                        {dayOptions}
+                    </select>
+                    <select className="date-year" name="year">
+                    <option value="None">Select Year:</option>
+                        {yearOptions}
+                    </select>
+                </div>
+                <div className="event-time">
+                    <label>Time:</label>
+                    <select className="time-hour" name="hour">{hoursOptions}</select>
+                    <select className="time-minute" name="minute">{minutesOptions}</select>
+                    <select className="time-meridiem" name="meridiem">{meridiemOptions}</select>
+                </div>
+            </>
+        )
+    }
+
     render(){
         return(
             <form onSubmit={this.handleFormSubmit} id="add-event-form">
@@ -45,28 +153,27 @@ export default class AddEvent extends React.Component{
                 <div className="info"><p>Please note: Once your event is created, you will only have the ability to update the title and description.</p></div>
                 <div>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" placeholder="Wine & Networking" name="title" required />
+                    <input type="text" id="title" placeholder="ex. Wine & Networking" name="title"  required  />
                 </div>
                 <div>
                     <label htmlFor="restaurant">Restaurant:</label>
-                    <input type="text" id="restaurant" placeholder="Water Grill" name="restaurant" required />
+                    <input type="text" id="restaurant" placeholder="ex. Water Grill" name="restaurant"  required  />
                 </div>
                 <div>
                     <label htmlFor="restaurant-address">Restaurant Address:</label>
-                    <input type="text" id="restaurant-address" placeholder="123 Restaurant Lane" name="restaurantAddress" required/>
+                    <input type="text" id="restaurant-address" placeholder="ex. 123 Restaurant Lane" name="restaurantAddress"  required  />
                 </div>
                 <div>
                     <label htmlFor="event-description">Description:</label>
-                    <textarea id="event-description" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis finibus orci, vel auctor dolor vulputate vitae. In fringilla tristique dui vitae blandit." name="eventDescription" required>
+                    <textarea id="event-description" placeholder="ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis finibus orci, vel auctor dolor vulputate vitae. In fringilla tristique dui vitae blandit." name="eventDescription"  required >
                     </textarea>
                 </div>
                 <div>
-                    <label htmlFor="event-date">Date:</label>
-                    <input type="datetime-local" id="event-date" name="eventDate" required />
+                    {this.dateInputCreator()}
                 </div>
                 <div>
                     <label htmlFor="purpose">Purpose:</label>
-                    <select id="purpose" name="purpose" required>
+                    <select id="purpose" name="purpose"  required >
                         <option value="Social">Social</option>
                         <option value="Networking">Networking</option>
                         <option value="Game Night">Game Night</option>
