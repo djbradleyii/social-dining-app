7e143960bcfa69a51c5a693c4fffaea2f7040edd
David B <d.jbradley.ii@gmail.com>
2020-01-24 12:58:52 -0800

    Updates error message and loading message handling to clear both upon submission

--- src/components/SignInPage/SignInPage.js
+++ src/components/SignInPage/SignInPage.js
@@ -9,6 +9,7 @@ import './SignInPage.css';
 export default class SignInPage extends React.Component{
     static contextType = ContextManager;
     handleSubmitJwtAuth = e => {
+        this.context.clearErrorMessage();
         this.context.updateLoadingMessage('Loading...');
         e.preventDefault();
         this.setState({ error: null });
