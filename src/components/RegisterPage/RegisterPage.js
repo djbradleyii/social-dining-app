d50486f576396f0bdf98672cc80f4bc261bb4b3b
David B <d.jbradley.ii@gmail.com>
2023-10-18 16:16:34 -0700

    Updates error handling and fixes some eslint issues

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,123 +1,197 @@
-import React from 'react';
-import './RegisterPage.css';
-import ContextManager from '../../context/context-manager';
-import AuthApiService from '../../services/auth-api-service';
+import React from "react";
+import "./RegisterPage.css";
+import ContextManager from "../../context/context-manager";
+import AuthApiService from "../../services/auth-api-service";
 
-export default class RegisterPage extends React.Component{
-    static contextType = ContextManager;
-    
-        handleRegSubmit = (e) => {
-            this.context.updateLoadingMessage('Loading...');
-            e.preventDefault();
-            this.setState({ error: null });
-            const { fname, lname, dob, gender, marital_status, occupation, bio, email, password, passwordVerify } = e.target;
-            const { history } = this.props;
-            
-            let newUser = {
-                fname: fname.value,
-                lname: lname.value,
-                dob: dob.value,
-                gender: gender.value,
-                marital_status: marital_status.value,
-                occupation: occupation.value,
-                bio: bio.value,
-                email: email.value,
-                password: password.value
-            }
+export default class RegisterPage extends React.Component {
+  static contextType = ContextManager;
 
-            if(password.value === passwordVerify.value){
-                AuthApiService.postUser(newUser)
-                .then((user) => {
-                    fname.value = '';
-                    lname.value = '';
-                    dob.value = '';
-                    gender.value = '';
-                    marital_status.value = '';
-                    occupation.value = '';
-                    bio.value = '';
-                    email.value = '';
-                    password.value = '';
-                    this.context.clearErrorMessage();
-                    history.push(`/registered`);
-                })
-                .catch(res => {
-                    this.context.clearLoadingMessage();
-                    this.context.updateErrorMessage('Oops: '+ res.error);
-                    this.context.scrollToErrorMessage();
-                })
-            } else {
-                this.context.clearLoadingMessage();
-                this.context.updateErrorMessage('Password must match');
-                this.context.scrollToErrorMessage();
-            }
-        }
+  handleRegSubmit = (e) => {
+    this.context.updateLoadingMessage("Loading...");
+    e.preventDefault();
+    this.setState({ error: null });
+    const {
+      fname,
+      lname,
+      dob,
+      gender,
+      marital_status,
+      occupation,
+      bio,
+      email,
+      password,
+      passwordVerify,
+    } = e.target;
+    const { history } = this.props;
 
-        componentWillUnmount(){
-            this.context.clearLoadingMessage();
-            this.context.clearErrorMessage();
-        }
+    let newUser = {
+      fname: fname.value,
+      lname: lname.value,
+      dob: dob.value,
+      gender: gender.value,
+      marital_status: marital_status.value,
+      occupation: occupation.value,
+      bio: bio.value,
+      email: email.value,
+      password: password.value,
+    };
 
-    render(){
-        return(
-            <form id="register-form" onSubmit={this.handleRegSubmit}>
-                <div className="info">*Required Fields</div>
-                <div className="error-message">{!!this.context.errorMessage && this.context.errorMessage}</div>
-                <div>
-                    <label htmlFor="fname">*First Name:</label>
-                    <input type="text" id="fname" placeholder="ex. Ken" name="fname" required />
-                </div>
-                <div>
-                    <label htmlFor="lname">*Last Name:</label>
-                    <input type="text" id="lname" placeholder="ex. Adams" name="lname" required/>
-                </div>
-                <div>
-                    <label htmlFor="dob">*Birth Date:</label>
-                    <input type="date" id="dob" name="dob" required/>
-                </div>
-                <div>
-                    <fieldset className="radiogroup">
-                        <legend>*Gender</legend>
-                        <ul className="radio">
-                            <li><input type="radio" name="gender" id="male" value="Male" /><label htmlFor="male">Male</label></li>
-                            <li><input type="radio" name="gender" id="female" value="Female" defaultChecked /><label htmlFor="female">Female</label></li>
-                        </ul>
-                    </fieldset>
-                </div>
-                <div>
-                    <label htmlFor="marital_status">*Marital Status:</label>
-                    <select id="marital_status" required>
-                        <option value="Single" name="marital_status">Single</option>
-                        <option value="Married" name="marital_status">Married</option>
-                        <option value="Divorced" name="marital_status">Divorced</option>
-                        <option value="Separated" name="marital_status">Separated</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="occupation">*Occupation:</label>
-                    <input type="text" id="occupation" name="occupation" placeholder="ex. Lawyer" required/>
-                </div>
-                <div>
-                    <label htmlFor="user-summary">Bio:</label>
-                    <textarea id="user-summary" name="bio"></textarea>
-                </div>
-                <div>
-                    <label htmlFor="email">*Email:</label>
-                    <input type="email" id="email" name="email" placeholder="ex. myemailaddress@email.com" required />
-                </div>
-                <div>
-                    <div><p className="info info-password">Your password must contain 1 number, 1 capital letter, 1 special character (!@#$%^&amp;) and it must be at least 8 characters long.</p></div>
-                    <label htmlFor="password">*Password:</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <div>
-                    <label htmlFor="passwordVerify">*Verify Password:</label>
-                    <input type="password" id="passwordVerify" name="passwordVerify" required />
-                </div>
-                <div className="loading-message">{!!this.context.loadingMessage && this.context.loadingMessage}</div>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>    
-        </form>
-        );
+    if (password.value === passwordVerify.value) {
+      AuthApiService.postUser(newUser)
+        .then((user) => {
+          fname.value = "";
+          lname.value = "";
+          dob.value = "";
+          gender.value = "";
+          marital_status.value = "";
+          occupation.value = "";
+          bio.value = "";
+          email.value = "";
+          password.value = "";
+          this.context.clearErrorMessage();
+          history.push(`/registered`);
+        })
+        .catch((res) => {
+          this.context.clearLoadingMessage();
+          this.context.updateErrorMessage("Oops: " + res.error?.message);
+          this.context.scrollToErrorMessage();
+        });
+    } else {
+      this.context.clearLoadingMessage();
+      this.context.updateErrorMessage("Password must match");
+      this.context.scrollToErrorMessage();
     }
+  };
+
+  componentWillUnmount() {
+    this.context.clearLoadingMessage();
+    this.context.clearErrorMessage();
+  }
+
+  render() {
+    console.log(
+      "%cRegisterPage.js line:59 this.context.errorMessage",
+      "color: #007acc;",
+      this.context.errorMessage
+    );
+    return (
+      <form id="register-form" onSubmit={this.handleRegSubmit}>
+        <div className="info">*Required Fields</div>
+        <div className="error-message">
+          {!!this.context.errorMessage && this.context.errorMessage.message}
+        </div>
+        <div>
+          <label htmlFor="fname">*First Name:</label>
+          <input
+            type="text"
+            id="fname"
+            placeholder="ex. Ken"
+            name="fname"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lname">*Last Name:</label>
+          <input
+            type="text"
+            id="lname"
+            placeholder="ex. Adams"
+            name="lname"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="dob">*Birth Date:</label>
+          <input type="date" id="dob" name="dob" required />
+        </div>
+        <div>
+          <fieldset className="radiogroup">
+            <legend>*Gender</legend>
+            <ul className="radio">
+              <li>
+                <input type="radio" name="gender" id="male" value="Male" />
+                <label htmlFor="male">Male</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="gender"
+                  id="female"
+                  value="Female"
+                  defaultChecked
+                />
+                <label htmlFor="female">Female</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+        <div>
+          <label htmlFor="marital_status">*Marital Status:</label>
+          <select id="marital_status" required>
+            <option value="Single" name="marital_status">
+              Single
+            </option>
+            <option value="Married" name="marital_status">
+              Married
+            </option>
+            <option value="Divorced" name="marital_status">
+              Divorced
+            </option>
+            <option value="Separated" name="marital_status">
+              Separated
+            </option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="occupation">*Occupation:</label>
+          <input
+            type="text"
+            id="occupation"
+            name="occupation"
+            placeholder="ex. Lawyer"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="user-summary">Bio:</label>
+          <textarea id="user-summary" name="bio"></textarea>
+        </div>
+        <div>
+          <label htmlFor="email">*Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="ex. myemailaddress@email.com"
+            required
+          />
+        </div>
+        <div>
+          <div>
+            <p className="info info-password">
+              Your password must contain 1 number, 1 capital letter, 1 special
+              character (!@#$%^&amp;) and it must be at least 8 characters long.
+            </p>
+          </div>
+          <label htmlFor="password">*Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div>
+          <label htmlFor="passwordVerify">*Verify Password:</label>
+          <input
+            type="password"
+            id="passwordVerify"
+            name="passwordVerify"
+            required
+          />
+        </div>
+        <div className="loading-message">
+          {!!this.context.loadingMessage && this.context.loadingMessage}
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
